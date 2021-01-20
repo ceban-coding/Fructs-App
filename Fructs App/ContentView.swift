6bc686c2a923d2410285cec2f2097c1204a6036e
Ion Ceban <ceban.ion1992@gmail.com>
2021-01-20 10:40:01 -0600

    Initial Commit

---
+++ Fructs App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructs App
+//
+//  Created by Ion Ceban on 1/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructs App/Fructs_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fructs_AppApp.swift
+//  Fructs App
+//
+//  Created by Ion Ceban on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Fructs_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
